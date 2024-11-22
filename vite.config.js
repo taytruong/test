a9a6041d6016a71e357a91f9ce4cc0ab38faacce
taytruong <taytruong05@gmail.com>
2024-11-22 21:41:37 +0700

    fix: third

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/test-growth-intern/", // Đặt base URL theo tên repository trên GitHub
+  base: "/test-growth-intern/", 
 });
