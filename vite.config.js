80a40edc86e831a9c7cf0413db1067bc80c5dbe0
taytruong <taytruong05@gmail.com>
2024-11-23 11:45:36 +0700

    fix: config vite js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          BASE_URL: "/test-growth-engineer/",
+        },
+      },
+    }),
+  ],
   base: "/test-growth-engineer/",
 });
