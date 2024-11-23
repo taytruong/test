3c6db230c084d74ee77eec72f967bdd88776b9bd
taytruong <taytruong05@gmail.com>
2024-11-23 11:52:14 +0700

    fix: hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Questions from "./pages/Questions.jsx";
 import Result from "./pages/Result.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter
-      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
-    >
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/questions" element={<Questions />} />
         <Route path="/result" element={<Result />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
